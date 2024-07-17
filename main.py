f6e1a5edf5b1e80d14bd190df266d7ae42842113
Anton Kurtin <norflin321@gmail.com>
2024-07-17 10:52:10 +0300

    multiprocess

--- main.py
+++ main.py
@@ -1,11 +1,33 @@
-import gradio as gr
+import multiprocessing
+
+# 09:14
 
 def greet(name):
-    return f"Hello {name}!"
+  return f"Hello {name}!"
+
+def on_closed():
+  print("quit")
+  quit()
+
+def gradio_process_worker():
+  import gradio as gr
+  import torch
+  print(torch.rand(5, 3))
+  print("ready")
+
+def webview_process_worker():
+  import webview
+  main_window = webview.create_window("Woah dude!", html="<h1>Loading...<h1>")
+  main_window.events.closed += on_closed
+  webview.start()
 
 def main():
-  ui = gr.Interface(fn=greet, inputs="text", outputs="text", allow_flagging="never")
-  ui.launch()
+  gradio_process = multiprocessing.Process(target=gradio_process_worker)
+  webview_process = multiprocessing.Process(target=webview_process_worker)
 
-if __name__ == "__main__":
-  main()
+  gradio_process.start()
+  webview_process.start()
+  webview_process.join()
+  gradio_process.join()
+
+if __name__ == "__main__": main()
--- readme.md
+++ readme.md
@@ -1,6 +1,22 @@
-- 
-  ```
-  pip install gradio
-  pip install pyinstaller 
-  ```
-- To build Windows 10 executable run `pyinstaller main.spec` or remove spec and build files and follow these steps https://github.com/pyinstaller/pyinstaller/issues/8108#issuecomment-1815298117
+```
+preferably create fresh venv
+
+pip install gradio
+pip install pyinstaller
+pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu121
+pip uninstall numpy
+pip install "numpy<2.0"
+pip install pythonnet
+pip install pywebview
+pip install asyncio
+
+pyi-makespec --collect-data=gradio_client --collect-data=gradio --onefile --windowed main.py
+a = Analysis(
+    ...
+    module_collection_mode={
+        'gradio': 'py',  # Collect gradio package as source .py files
+    },
+)
+
+pyinstaller main.spec
+```
